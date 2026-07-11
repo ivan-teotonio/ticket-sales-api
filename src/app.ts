9ccdea52284a0277fc2157a1ae64b681c644eb36
ivan-teotonio <ivanteotonio256@gmail.com>
2026-07-10 22:54:57 -0300

    feat(events): adiciona endpoints base para criação dos eventos

--- src/app.ts
+++ src/app.ts
@@ -13,6 +13,18 @@ app.post("/auth/login", (req, res) => {
   res.send();
 });
 
+app.post("/events", (req, res) => {
+  const { name, description, date, address, phone } = req.body;
+});
+
+app.get("/events", (req, res) => {});
+
+app.get("/events/:eventId", (req, res) => {
+  const { eventId } = req.params;
+  console.log(eventId);
+  res.send();
+});
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
