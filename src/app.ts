1090ffb99115d000df422e5ad1ae499555df155f
ivan-teotonio <ivanteotonio256@gmail.com>
2026-07-10 23:38:21 -0300

    feat: adicionado endpoit dos eventos dos parceiros

--- src/app.ts
+++ src/app.ts
@@ -13,6 +13,22 @@ app.post("/auth/login", (req, res) => {
   res.send();
 });
 
+app.post("/partners", (req, res) => {
+  const { name, email, password, company_name } = req.body;
+});
+
+app.get("/partners/events", (req, res) => {});
+
+app.get("/partners/events/:eventId", (req, res) => {
+  const { eventId } = req.params;
+  console.log(eventId);
+  res.send();
+});
+
+app.post("/customers", (req, res) => {
+  const { name, email, password, address, telefone } = req.body;
+});
+
 app.post("/events", (req, res) => {
   const { name, description, date, address, phone } = req.body;
 });
